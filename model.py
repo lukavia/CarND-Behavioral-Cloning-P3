c9d40c1e41aa50c1d12a8ec1e475d3a8cd36691c
Anton Avramov <lukav@lukav.com>
2017-12-02 11:39:58 +0000

    Added generator for processing the dataset in batches and ploting the result of the training

--- model.py
+++ model.py
@@ -1,38 +1,55 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
 
-lines = []
+# Read the data images
+samples = []
+corrections = [0.0, 0.25, -0.25] # used for center, left, right imag
 with open('data/driving_log.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  next(reader)
-  for line in reader:
-    lines.append(line)
+    reader = csv.reader(csvfile)
+    # Skip the header
+    next(reader)
+    for line in reader:
+        for i in range(3):
+            angle = float(line[3]) + corrections[i]
+            samples.append([line[i], angle])
 
-images = []
-measurements = []
-corrections = [0.0, 0.22, -0.22]
-for line in lines:
-  for i in range(3):
-    source_path = line[i]
-    filename = source_path.split('/')[-1]
-    current_path = 'data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3]) + corrections[i]
-    measurements.append(measurement)
-    # Flip Image
-    images.append(cv2.flip(image, 1))
-    measurements.append(measurement * -1.0)
-  
+# Split the samples for training and validation
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        np.random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                    source_path = batch_sample[0]
+                    filename = source_path.split('/')[-1]
+                    current_path = 'data/IMG/' + filename
+                    image = cv2.imread(current_path)
+                    images.append(image)
+                    #measurement = float(line[3]) + corrections[i]
+                    measurement = batch_sample[1]
+                    measurements.append(measurement)
+                    # Flip Image
+                    images.append(cv2.flip(image, 1))
+                    measurements.append(measurement * -1.0)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
@@ -46,9 +63,9 @@ model.add(Cropping2D(cropping=((70,25),(0,0))))
 #model.add(Dense(84))
 #model.add(Dense(1))
 
-model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
@@ -56,8 +73,29 @@ model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(1))
 
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
+history_object = model.fit_generator(train_generator, \
+            samples_per_epoch=(len(train_samples) * 2), \
+            validation_data=validation_generator, \
+            nb_val_samples=(len(validation_samples) * 2), \
+            nb_epoch=6, verbose=1)
 
 model.save('model.h5')
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
 exit()
