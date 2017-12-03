b2f9b67060852dbe247094958572d96d05374677
Anton Avramov <lukav@lukav.com>
2017-12-03 10:27:20 +0000

    Cleanup and comments

--- model.py
+++ model.py
@@ -5,13 +5,13 @@ import sklearn
 
 # Read the data images
 samples = []
-corrections = [0.0, 0.25, -0.25] # used for center, left, right imag
+corrections = [0.0, 0.25, -0.25] # used for center, left, right image
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     # Skip the header
     next(reader)
     for line in reader:
-        for i in range(3):
+        for i in range(3): # Add the 3 images with their corrections, so they can be suffled later
             angle = float(line[3]) + corrections[i]
             samples.append([line[i], angle])
 
@@ -34,10 +34,9 @@ def generator(samples, batch_size=32):
                     current_path = 'data/IMG/' + filename
                     image = cv2.imread(current_path)
                     images.append(image)
-                    #measurement = float(line[3]) + corrections[i]
                     measurement = batch_sample[1]
                     measurements.append(measurement)
-                    # Flip Image
+                    # Flip Image and add it to the batch also
                     images.append(cv2.flip(image, 1))
                     measurements.append(measurement * -1.0)
 
@@ -46,23 +45,16 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-import matplotlib.pyplot as plt
 
+# Preprocessing 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
-#model.add(Convolution2D(6,5,5,activation="relu"))
-#model.add(MaxPooling2D())
-#model.add(Convolution2D(6,5,5,activation="relu"))
-#model.add(MaxPooling2D())
-#model.add(Flatten())
-#model.add(Dense(120))
-#model.add(Dense(84))
-#model.add(Dense(1))
+# Crop first to work with less data
+model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
+# NVIDIA model
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
@@ -78,16 +70,14 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 model.compile(loss='mse', optimizer='adam')
+# Samples per epoch is * 2, cause for each batch we return double images (normal and flipped)
 history_object = model.fit_generator(train_generator, \
             samples_per_epoch=(len(train_samples) * 2), \
             validation_data=validation_generator, \
             nb_val_samples=(len(validation_samples) * 2), \
-            nb_epoch=6, verbose=1)
+            nb_epoch=3, verbose=1)
 
 model.save('model.h5')
-
-### print the keys contained in the history object
-print(history_object.history.keys())
 
 ### plot the training and validation loss for each epoch
 plt.plot(history_object.history['loss'])
